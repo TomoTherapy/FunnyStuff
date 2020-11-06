c06c3acccf0f7905fe1a3e6be8f48149a3415673
Joon-gi Hong <crazy_puddle@live.co.kr>
2020-11-06 22:24:44 +0900

    start!

---
+++ MousePointerChaser/MousePointerChaser/ViewModels/MainWindow_ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MousePointerChaser.ViewModels
+{
+    public class MainWindow_ViewModel : ViewModelBase
+    {
+        public int PositionX { get => Cursor.Position.X; set { PositionX = Cursor.Position.X; RaisePropertyChanged(nameof(PositionX)); } }
+        public int PositionY { get => Cursor.Position.Y; set { PositionY = Cursor.Position.Y; RaisePropertyChanged(nameof(PositionY)); } }
+
+        public MainWindow_ViewModel()
+        {
+            //Cursor.Position.X
+        }
+
+    }
+}
---
+++ MousePointerChaser/MousePointerChaser/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MousePointerChaser.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public void RaisePropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
